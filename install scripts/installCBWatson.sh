510a9229f10783936f81ac80c281c7647b8fe62b
bharath-gp <gpbharath@yahoo.in>
2015-11-03 14:10:46 +0530

    fixed the download path to take the correct path based on input

--- install scripts/installCBWatson.sh
+++ install scripts/installCBWatson.sh
@@ -23,5 +23,5 @@ echo $VERSIONNUMBER
 rpm -e couchbase-server
 rm -rf /tmp/rpm
 mkdir /tmp/rpm
-wget -O /tmp/rpm/couchbaseServer.rpm http://172.23.120.24/builds/latestbuilds/couchbase-server/watson/$VERSION/couchbase-server-enterprise-4.5.0-$VERSION-centos6.x86_64.rpm
+wget -O /tmp/rpm/couchbaseServer.rpm http://172.23.120.24/builds/latestbuilds/couchbase-server/$RELEASE/$VERSIONNUMBER/couchbase-server-enterprise-$VERSION-centos6.x86_64.rpm
 rpm -i /tmp/rpm/couchbaseServer.rpm
